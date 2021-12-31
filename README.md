2dc3afb5a5f4184e2c30f5cbf9059c35b7b5c00d
Mattyboy84 <41299838+mattyboy84@users.noreply.github.com>
2021-12-31 14:04:28 +0000

    continued testing with Facebook marketplace

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 Inial testing with Ebay:    
 ![](src\images\ebayScraper.png)
+Continued testing with Facebook marketplace:
+query for '3ds game'
+![](src\images\facebook3DsGame.png)
+query for 'car'
+![](src\images\facebookCar.png)
+
  
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,8 @@
 package sample;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
@@ -24,18 +27,20 @@ public class Main extends Application {
 
     ArrayList<Product> products = new ArrayList<>();
 
-    String siteURL = "https://www.ebay.co.uk/b/Nintendo-Switch-Video-Games/139973/bn_75532845";
+    //String siteURL = "https://www.ebay.co.uk/b/Nintendo-Switch-Video-Games/139973/bn_75532845";
+    StringBuilder siteURL = new StringBuilder("https://en-gb.facebook.com/marketplace/category/search/?query=");
 
-    //NINTENDO SWITCH GAMES - https://www.ebay.co.uk/b/Nintendo-Switch-Video-Games/139973/bn_75532845
-
-    //ELEMENT title - s-item__title s-item__title--has-tags
-    //BASE TITLE ELEMENT - s-item__title
-//"https://www.ebay.co.uk/b/Nintendo-Switch-Video-Games/139973/bn_75532845"
+    //FACEBOOK - "https://en-gb.facebook.com/marketplace/item/" + ID + "/" - this gives the product url
     @Override
     public void start(Stage stage) throws Exception {
-
+        //
+        String searchProduct = "3ds games";
+        searchProduct = searchProduct.replaceAll(" ", "+");//facebook has a space in its query. A "+" can be used instead.
+        siteURL.append(searchProduct);
+        //
+        //
         System.out.println("Site: " + siteURL);
-        URL websiteUrl = new URL(siteURL);
+        URL websiteUrl = new URL(siteURL.toString());
         HttpURLConnection urlConn = (HttpURLConnection) websiteUrl.openConnection();
         String line;
         StringBuilder sb = new StringBuilder();
@@ -45,28 +50,47 @@ public class Main extends Application {
         }
         Document doc = Jsoup.parse(sb.toString());
 
-        //Elements test = doc.getElementsByClass("s-item__info clearfix");
+        Elements elements = doc.getAllElements();
 
-        Elements items = doc.getElementsByClass("s-item__info clearfix");
-        for (Element product : items) {
-            if (!product.getElementsByClass("s-item__title-tag").toString().contains("SPONSORED")) {
-                String title = product.getElementsByClass("s-item__title").toString();
-                title = title.replaceAll("\\<.*?\\>", "");
-                title=title.replaceAll("amp;","");
-                //
-                String price = product.getElementsByClass("s-item__price").toString();
-                price = price.replaceAll("\\<.*?\\>", "");
-                //
-                String url = product.getElementsByClass("s-item__link").toString();
-                url = url.substring(url.indexOf("href=") + 6, url.indexOf("><h3") - 1);
+        //System.out.println(doc);
 
-                products.add(new Product(product, title, price, url));
-                products.get(products.size() - 1).output();
+        Element target = elements.get(elements.size()-6);
 
-                //System.out.println(product);
-                System.out.println("-------------------------------------------------------------");
+        //System.out.println(target);
+
+        String b=target.toString().substring(target.toString().indexOf("{\"__bbox"),target.toString().length()-21);
+
+        JsonObject wholeProductData = (JsonObject) new JsonParser().parse(b);
+
+        //System.out.println(wholeProductData);
+
+        JsonArray productData = wholeProductData.get("__bbox").getAsJsonObject().get("result").getAsJsonObject()
+                .get("data").getAsJsonObject().get("marketplace_search").getAsJsonObject().get("feed_units")
+                .getAsJsonObject().get("edges").getAsJsonArray();
+
+
+        for (int i = 0; i <productData.size() ; i++) {
+            String title=productData.get(i).getAsJsonObject().get("node").getAsJsonObject().get("listing").getAsJsonObject().get("marketplace_listing_title").getAsString();
+            String price = productData.get(i).getAsJsonObject().get("node").getAsJsonObject().get("listing").getAsJsonObject().get("listing_price").getAsJsonObject()
+                    .get("formatted_amount").getAsString();
+            String strikePrice = null;
+            try{//looks for a strike price
+                strikePrice = productData.get(i).getAsJsonObject().get("node").getAsJsonObject().get("listing").getAsJsonObject().get("strikethrough_price").getAsJsonObject()
+                        .get("formatted_amount").getAsString();
+
+            }catch (Exception e){
             }
+            String id = productData.get(i).getAsJsonObject().get("node").getAsJsonObject().get("listing").getAsJsonObject().get("id").getAsString();
+
+            products.add(new Product(title,price,strikePrice,id));
+            //
+            products.get(products.size()-1).output();
+            System.out.println("-------------------------------------");
         }
+
+        //System.out.println(productData);
+
+
 
         stage.setScene(scene);
         //stage.show();
